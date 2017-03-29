61fa623a4acb19947e7d580edbe6464d69fa9d00
Nikita Spivachuk <nikita.spivachuk@dsr-company.com>
2017-03-29 22:26:26 +0100

    Updated the warnings filter.

--- sovrin_client/test/conftest.py
+++ sovrin_client/test/conftest.py
@@ -50,7 +50,7 @@ from plenum.test.conftest import tdir, nodeReg, up, ready, \
 def warnfilters(plenum_warnfilters):
     def _():
         plenum_warnfilters()
-        warnings.filterwarnings('ignore', category=ResourceWarning, module='sovrin_client\.cli\.cli', message='unclosed file')
+        warnings.filterwarnings('ignore', category=DeprecationWarning, module='plenum\.common\.looper', lineno=179)
     return _
 
 
